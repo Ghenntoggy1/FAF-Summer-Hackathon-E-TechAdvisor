620187038b38c5fe5bad0ed598377ed18da13834
Ghenntoggy1 <roman.gusev@isa.utm.md>
2024-06-15 13:22:05 +0300

    init commit

---
+++ src/main/java/com/ETechAdvisor/ETechAdvisor/ETechAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.ETechAdvisor.ETechAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETechAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETechAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ETechAdvisor/ETechAdvisor/ETechAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ETechAdvisor.ETechAdvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETechAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
